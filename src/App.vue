85a21f747967e2e12864ab09f1a41c99bc22fa53
Skachkov Maxim <AngelSamAdd@gmail.com>
2025-05-04 22:04:24 +0300

    Add version label

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script>
+import VersionLabel from './components/VersionLabel.vue';
+
+export default {
+    components: {
+        VersionLabel
+    }
+}
+</script>
+
 <template>
+    <VersionLabel />
     <RouterView />
 </template>
--- src/storage/constants.js
+++ src/storage/constants.js
@@ -1,6 +1,8 @@
 export const API_URL = "https://dnd.radiored.ru";
 export const IMG_URL = "https://radiored.ru/images/sailpunk";
 
+export const VERSION = "0.1.2";
+
 export const POINT_COLOR = {
   town: "#3A76EA",
   province: "#ff0000",
