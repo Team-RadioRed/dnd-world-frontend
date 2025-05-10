c0bd8929d9584675eea794711740a61ed4527544
Skachkov Maxim <AngelSamAdd@gmail.com>
2025-05-11 00:28:45 +0300

    Update version

--- src/storage/constants.js
+++ src/storage/constants.js
@@ -1,7 +1,7 @@
 export const API_URL = "https://dnd.radiored.ru";
 export const IMG_URL = "https://radiored.ru/images/sailpunk";
 
-export const VERSION = "0.1.4";
+export const VERSION = "0.1.5";
 
 export const POINT_COLOR = {
   town: "#3A76EA",
