ae2ade21a67bb4f8408c9e9a446c768ea21bd6db
Skachkov Maxim <AngelSamAdd@gmail.com>
2025-11-29 14:47:38 +0300

    Fix local svg file

--- src/assets/scripts/images.js
+++ src/assets/scripts/images.js
@@ -1,11 +1,11 @@
 import { IMG_URL } from "@/storage/constants";
 
-const LOCAL_PATH = "../images";
+const LOCAL_PATH = "@/assets/images/";
 
 export function getImageServer(filePath, projectName = "worlds") {
   return `${IMG_URL}/${projectName}/${filePath}`;
 }
 
 export function getImageLocal(filePath) {
-  return new URL(`${LOCAL_PATH}/${filePath}`, import.meta.url).href;
+  return `/${filePath}`;
 }
